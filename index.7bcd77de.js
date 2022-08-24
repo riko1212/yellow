3d5bf2d890c44f83e15e006ed24eb891a119c3ef
riko1212 <46224717+riko1212@users.noreply.github.com>
2022-08-24 16:26:44 +0000

    Deploying to gh-pages from @ riko1212/yellow@91bcbc11dc5b452a5a2b5c99131ea31a97b3f3c4 🚀

---
+++ index.7bcd77de.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector(".menu-open"),modal:document.querySelector(".menu")};e.openModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=index.7bcd77de.js.map
---
+++ index.efe67234.js
@@ -0,0 +1,2 @@
+!function(){var e;(e={openModalBtn:document.querySelector(".menu-open"),modal:document.querySelector(".menu")}).openModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.efe67234.js.map
